7e147e7887c0247803c03ac0adba5e67e1900869
Aki Gao <xag9bb@virginia.edu>
2017-06-22 22:34:02 -0400

    configure main plan to use geth module

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
 module "geth" {
   source = "./modules/geth"
+
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region = "${var.region}"
+  availability_zone = "${var.availability_zone}"
+  instance_type = "${var.instance_type}"
+  ami = "${var.ami}"
+  private_key_path = "${var.private_key_path}"
 }
