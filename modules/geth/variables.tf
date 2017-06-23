0a1bf4707d81d55e1ca5572f0bff178c88262503
Aki Gao <xag9bb@virginia.edu>
2017-06-22 23:00:42 -0400

    update variables file to reflect modularization of the geth plan

--- modules/geth/variables.tf
+++ modules/geth/variables.tf
@@ -1,17 +1,7 @@
 variable "access_key" {}
 variable "secret_key" {}
-variable "region" {
-  default = "us-east-1"
-}
-variable "availability_zone" {
-  default = "a"
-}
-variable "instance_type"{
-  default = "t2.micro"
-}
-variable "ami"{
-  default = "ami-2b06283d"
-}
-variable "private_key_path"{
-  default = "~/.ssh/id_rsa"
-}
+variable "region" {}
+variable "availability_zone" {}
+variable "instance_type"{}
+variable "ami"{}
+variable "private_key_path"{}
