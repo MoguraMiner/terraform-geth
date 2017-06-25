29c7a1d12c0c2a19e292078bbc663ffc75d254ad
Aki Gao <xag9bb@virginia.edu>
2017-06-25 15:19:38 -0400

    update security group to allow outbound traffic on port 8545 over udp & tcp

--- modules/spot_fleet_request/main.tf
+++ modules/spot_fleet_request/main.tf
@@ -43,19 +43,32 @@ resource "aws_security_group" "ethminer-security-group" {
     protocol = "TCP"
     cidr_blocks = ["0.0.0.0/0"]
   }
-}
 
-resource "aws_spot_fleet_request" "ethminers" {
-  iam_fleet_role = "${aws_iam_role.spot_fleet_role.arn}"
-  spot_price = "${var.spot_price}"
-  allocation_strategy = "${var.allocation_strategy}"
-  target_capacity = "${var.capacity}"
-
-  launch_specification {
-    instance_type = "${var.instance_type}"
-    ami = "${var.ami}"
-    key_name = "personal-key"
-    user_data = "${file("${path.module}/mine.sh")}"
-    vpc_security_group_ids = ["${aws_security_group.ethminer-security-group.id}"]
+  ingress {
+    from_port = 8545
+    to_port = 8545
+    protocol = "UDP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 8545
+    to_port = 8545
+    protocol = "UDP"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+# resource "aws_spot_fleet_request" "ethminers" {
+#   iam_fleet_role = "${aws_iam_role.spot_fleet_role.arn}"
+#   spot_price = "${var.spot_price}"
+#   allocation_strategy = "${var.allocation_strategy}"
+#   target_capacity = "${var.capacity}"
+#
+#   launch_specification {
+#     instance_type = "${var.instance_type}"
+#     ami = "${var.ami}"
+#     key_name = "personal-key"
+#     user_data = "${file("${path.module}/mine.sh")}"
+#     vpc_security_group_ids = ["${aws_security_group.ethminer-security-group.id}"]
+#   }
+# }
